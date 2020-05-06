60e2917852073f7a8e7f715d00b48f945eab8ee8
sh1rokovs <hilallod@yandex.ru>
2020-05-06 20:10:28 +0300

    111

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+void main()
+{
+	calc(5, 6);
+	bit_op(5, 6);
+	x_ord(5, 6);
+	cout << "---------" << endl;
+	random_sim();
+	system("pause");
+}
